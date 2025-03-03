efc893bdb69ddc611083bcbd89bf3e8bf6892721
elvis <elvisotieno24@gmail.com>
2025-03-03 13:04:33 +0300

    adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution in this file!
+// Declare companyName as a const
+const companyName = 'Scuber';
+
+// Declare mostProfitableNeighborhood using let and assign it to 'Chelsea'
+let mostProfitableNeighborhood = 'Chelsea';
+
+// Declare companyCeo using let and assign it to 'Susan Smith'
+let companyCeo = 'Susan Smith';
